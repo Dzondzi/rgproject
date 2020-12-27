0332748502e05b4359957385b220322ff69378fc
Dzondzi <nikola.borjan@yahoo.com>
2020-12-27 23:41:23 +0100

    map01 added

--- include/rg/Texture2D.h
+++ include/rg/Texture2D.h
@@ -19,14 +19,15 @@ public:
         glBindTexture(GL_TEXTURE_2D, tex);
 
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        // set texture wrapping to GL_REPEAT (default wrapping method)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
         // set texture filtering parameters
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
         int width, height, nrChannels;
         stbi_set_flip_vertically_on_load(true);
         unsigned char *data = stbi_load((FileSystem::getPath(path)).c_str(), &width, &height, &nrChannels, 0);
+
         GLint type;
         if (nrChannels == 1)
             type = GL_RED;
@@ -34,14 +35,13 @@ public:
             type = GL_RGB;
         else if (nrChannels == 4)
             type = GL_RGBA;
-        if (data)
-        {
+
+        if (data){
             glTexImage2D(GL_TEXTURE_2D, 0, type, width, height, 0, type, GL_UNSIGNED_BYTE, data);
             //std::cout << tex << "\n";
             glGenerateMipmap(GL_TEXTURE_2D);
         }
-        else
-        {
+        else{
             std::cout << FileSystem::getPath(path) << "\n";
             std::cout << "Failed to load texture" << std::endl;
             exit(EXIT_FAILURE);
@@ -50,9 +50,18 @@ public:
 
     }
 
-    void ourglTexParameteri(GLenum target, GLenum pname, const GLint *params){
-        glTexParameteri(target, pname, *params);
+    void setWrappingST(GLint param1, GLint param2){
+        activateTexture(this->tex);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, param1);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, param2);
     }
+
+    void setFilteringMinMag(GLint param1, GLint param2){
+        activateTexture(tex);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, param1);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, param2);
+    }
+
 
     void activateTexture(int value){
         glActiveTexture(GL_TEXTURE0 + value);
--- src/StartingCallbackInit.cpp
+++ src/StartingCallbackInit.cpp
@@ -9,6 +9,8 @@
 #include <rg/StartingCallbackInit.h>
 #include <rg/StartingGlfwInit.h>
 
+unsigned int width = 800;
+unsigned int height = 600;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -29,6 +31,19 @@ void key_callback(GLFWwindow *window, int key, int scancode, int action, int mod
     if (key == GLFW_KEY_G && action == GLFW_PRESS) {
         std::cerr << "G\n";
         glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
+    }
+    if (key == GLFW_KEY_O && action == GLFW_PRESS) {
+        std::cerr << "O\n";
+        height*=1.02;
+        width*=1.02;
+        glfwSetWindowSize(window, width, height);
+    }
+
+    if (key == GLFW_KEY_F && action == GLFW_PRESS) {
+        glfwMaximizeWindow(window);
+    }
+    if (key == GLFW_KEY_R && action == GLFW_PRESS) {
+        glfwRestoreWindow(window);
     }
 
 }
