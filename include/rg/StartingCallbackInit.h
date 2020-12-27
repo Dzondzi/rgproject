99fe00a8298462874e12c8d766fb2a34de9b8048
Dzondzi <nikola.borjan@yahoo.com>
2020-12-27 21:40:29 +0100

    pocetno

---
+++ include/rg/StartingCallbackInit.h
@@ -0,0 +1,13 @@
+//
+// Created by dzondzi on 27.12.20..
+//
+
+#ifndef PROJECT_BASE_STARTINCALLBACKINIT_H
+#define PROJECT_BASE_STARTINCALLBACKINIT_H
+
+void startCallbackInit();
+// tell GLFW to capture our mouse
+
+
+
+#endif //PROJECT_BASE_STARTINCALLBACKINIT_H
