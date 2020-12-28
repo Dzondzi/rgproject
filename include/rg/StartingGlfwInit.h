2de917ea0db14bbe88827d2c80b361c502b5f893
Dzondzi <nikola.borjan@yahoo.com>
2020-12-28 10:05:24 +0100

    added glfw init

---
+++ include/rg/StartingGlfwInit.h
@@ -0,0 +1,22 @@
+//
+// Created by dzondzi on 27.12.20..
+//
+
+#ifndef PROJECT_BASE_STARTINGGLFWINIT_H
+#define PROJECT_BASE_STARTINGGLFWINIT_H
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <stb_image.h>
+
+#define SCR_WIDTH 800
+#define SCR_HEIGHT 600
+
+extern GLFWwindow* window;
+
+void ourGlfwInit();
+void processInput(GLFWwindow *window);
+
+
+#endif //PROJECT_BASE_STARTINGGLFWINIT_H
---
+++ src/StartingGlfwInit.cpp
@@ -0,0 +1,58 @@
+//
+// Created by dzondzi on 27.12.20..
+//
+
+#include <rg/StartingGlfwInit.h>
+
+
+// glfw: initialize and configure
+// ------------------------------
+GLFWwindow* window;
+
+void processInput(GLFWwindow* window);
+
+void ourGlfwInit(){
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    #ifdef __APPLE__
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    #endif
+
+    window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    glfwMakeContextCurrent(window);
+
+// tell GLFW to capture our mouse
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+// glad: load all OpenGL function pointers
+// ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+// tell stb_image.h to flip loaded texture's on the y-axis (before loading model).
+    stbi_set_flip_vertically_on_load(true);
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+
+
+
